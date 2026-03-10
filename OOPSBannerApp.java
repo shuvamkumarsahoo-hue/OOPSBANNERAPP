d3fcecc6f079819db10f70bb7c8120232487008e
shuvamkumarsahoo-hue <shuvamkumarsahoo@gmail.com>
2026-03-10 09:13:54 +0530

    coded UC5 to print OOPS

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,17 +1,28 @@
+/**
+ * OOPSBannerApp UC5 - Render OOPS as Banner using Inline Array Initialization
+ *
+ * This use case extends UC4 by defining and populating the String array in a more
+ * concise way at the time of declaration using String.join() method to create each
+ * line of the banner. This further enhances code readability and maintainability.
+ *
+ * @author Developer
+ * @version 5.0
+ */
+
 public class OOPSBannerApp {
+    // Main method to run the banner display
     public static void main(String[] args) {
-
-
-        String[] lines = new String[7];
-
-        lines[0] = "  *****     *****     *****      *****  ";
-        lines[1] = " **     **  **     **  **     **  **     **";
-        lines[2] = " **     **  **     **  **     **  **      ";
-        lines[3] = " **     **  **     **  *****      *****  ";
-        lines[4] = " **     **  **     **  **              **";
-        lines[5] = " **     **  **     **  **        **     **";
-        lines[6] = "  *****     *****      **          *****  ";
-
+        String[] lines = {
+            String.join("  ", "  ***  ", "  ***  ", "******", " *****"),
+            String.join("  ", " **  **", " **  **", "**   **", "**    "),
+            String.join("  ", "**    **", "** **  ", "**  **", "**    "),
+            String.join("  ", "**    **", "** **  ", "**  **", " ***  "),
+            String.join("  ", "**    **", "** **  ", "******", "   ***"),
+            String.join("  ", "**    **", "** **  ", "**    ", "    **"),
+            String.join("  ", "**    **", "** **  ", "**    ", "    **"),
+            String.join("  ", " **  **", " **  **", "**    ", "**  **"),
+            String.join("  ", "  ***  ", "  ***  ", "**    ", " *****")
+        };
         for (String line : lines) {
             System.out.println(line);
         }
